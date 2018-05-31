71fa2dd39279f6610e95f8cebbf7480c22755382
youareboy <35476244+youareboy@users.noreply.github.com>
2018-05-31 12:44:29 +0800

    Update centos7.2_tsinghua_yum.sh

--- centos7.2_tsinghua_yum.sh
+++ centos7.2_tsinghua_yum.sh
@@ -13,7 +13,7 @@ rpm -e python-urlgrabber* --nodeps
 rpm -ivh python-iniparse-0.4-9.el7.noarch.rpm
 rpm -ivh python-urlgrabber-3.10-8.el7.noarch.rpm
 rpm -ivh yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
-rpm -ivh yum-3.4.3-158.el7.centos.noarch.rpm yum-plugin-fastestmirror-1.1.31-45.el7.noarch.rpm
+rpm -ivh yum-3.4.3-132.el7.centos.0.1.noarch.rpm yum-plugin-fastestmirror-1.1.31-45.el7.noarch.rpm
 cat >>/etc/yum.repos.d/yum.repo<<eof
 [localyum]
 name=centos7
